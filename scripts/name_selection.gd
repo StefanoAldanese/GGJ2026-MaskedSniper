9b9b82632e019301a3cf5e0c58537a2bf814b4b2
andreacopellino1 <andreacopellinoda@gmail.com>
2026-01-29 10:25:38 +0100

    Added UI in game, and new property to PlayerData, day

--- scripts/name_selection.gd
+++ scripts/name_selection.gd
@@ -14,6 +14,7 @@ func _on_yes_pressed() -> void:
 	if name.is_empty() == false:
 		PlayerData.current_nickname = name
 		PlayerData.current_score = 0
+		PlayerData.current_day = 1
 		get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 func _on_no_pressed() -> void:
--- scripts/player_data.gd
+++ scripts/player_data.gd
@@ -2,3 +2,4 @@ extends Node
 
 var current_nickname: String
 var current_score: int
+var current_day: int
