2bce27b2fae327e518c2e39a410b7482675efb51
andreacopellino1 <andreacopellinoda@gmail.com>
2026-01-30 19:50:48 +0100

    efe
    changed main menu, fixed credits, now with a back button too, added commands tips

--- scripts/credits_section.gd
+++ scripts/credits_section.gd
@@ -9,3 +9,23 @@ extends VBoxContainer
 func _ready() -> void:
 	section_name.text = section_title
 	section_contents.text = section_text
+
+
+extends MarginContainer
+
+@onready var button_start: TextureButton = $MarginContainer/VBoxContainer/VBoxContainer/ButtonStart
+@onready var button_scores: TextureButton = $MarginContainer/VBoxContainer/VBoxContainer/ButtonScores
+@onready var button_credits: TextureButton = $MarginContainer/VBoxContainer/VBoxContainer/ButtonCredits
+
+func _ready() -> void:
+	button_start.pressed.connect(_on_start_pressed)
+	button_scores.pressed.connect(_on_scores_pressed)
+	button_credits.pressed.connect(_on_credits_pressed)
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UIStuff/name-selection.tscn")
+
+func _on_scores_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UIStuff/Scores.tscn")
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UIStuff/credits.tscn")
